46f4b4bafc25b4ada43bdd2d98e634b538a57a98
didepanlayar <albinmaulana@yahoo.com>
2025-08-07 09:34:49 +0700

    Feature: executables under Docker for the VSCode environment

---
+++ src/npm-docker.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This shell is for creating npm executables under Docker for the VSCode environment.
+# Save to /usr/local/bin/npm and make it executable.
+
+# Docker directory, example: /Users/<username/Website/Docker
+DOCKER_COMPOSE_DIR="/path/to/Docker"
+
+# Host project directory.
+HOST_PROJECT_PATH=$(pwd)
+
+# Project directory, if Docker is in /Users/<username/Website/Docker then project is in /Users/<username/Website/Project/
+# Your app must be in /Users/<username/Website/Project/{App_1, App_2, etc...}.
+RELATIVE_PATH="${HOST_PROJECT_PATH#*path/to/project/}"
+
+ESCAPED_ARGS=""
+for arg in "$@"; do
+  ESCAPED_ARGS="$ESCAPED_ARGS \"$(printf '%q' "$arg")\""
+done
+
+COMMAND="cd $RELATIVE_PATH && npm $ESCAPED_ARGS"
+
+docker-compose -f "$DOCKER_COMPOSE_DIR/docker-compose.yml" exec workspace sh -c "$COMMAND"
